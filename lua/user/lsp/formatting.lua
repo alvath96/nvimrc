522b0c91b6396b86ad043659e76e5cda2048a7e8
Alva Thomson <alvathomson99@gmail.com>
2022-04-25 18:10:34 +0800

    fix Format() offset_encoding breaking API changes

--- lua/user/lsp/formatting.lua
+++ lua/user/lsp/formatting.lua
@@ -1,11 +1,11 @@
 function OrganizeImports(wait_ms)
   local params = vim.lsp.util.make_range_params()
-  params.context = {only = {"source.organizeImports"}}
+  params.context = { only = { "source.organizeImports" } }
   local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
   for _, res in pairs(result or {}) do
-    for _, r in pairs(res.result or {}) do
+    for client_id, r in pairs(res.result or {}) do
       if r.edit then
-        vim.lsp.util.apply_workspace_edit(r.edit)
+        vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.get_client_by_id(client_id).offset_encoding)
       else
         vim.lsp.buf.execute_command(r.command)
       end
@@ -13,10 +13,8 @@ function OrganizeImports(wait_ms)
   end
 end
 
-
 function Format()
   vim.api.nvim_command("lua vim.lsp.buf.formatting_sync()")
 
   vim.api.nvim_command('lua OrganizeImports()')
 end
-
